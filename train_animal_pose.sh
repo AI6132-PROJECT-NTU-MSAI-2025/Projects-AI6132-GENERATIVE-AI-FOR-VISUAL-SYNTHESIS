05a3457b3613f69c0af50619f64e0f13c6069c8b
柏森 <2417508347@qq.com>
2025-11-06 23:35:13 +0800

    add train_animal_pose.sh

---
+++ train_animal_pose.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+CUDA_LAUNCH_BLOCKING=1 HF_HOME="/hy-tmp/hg_cache" \
+accelerate launch train_animal_pose.py \
+--pretrained_model_name_or_path \
+stabilityai/stable-diffusion-xl-base-1.0 \
+--output_dir experiments/adapter_pose_xl_debug \
+--config configs/train/Adapter-XL-pose.yaml \
+--mixed_precision="fp16" \
+--resolution=1024 \
+--learning_rate=1e-5 \
+--max_train_steps=60000 \
+--train_batch_size=2 \
+--gradient_accumulation_steps=4 \
+--report_to="tensorboard" \
+--seed=42 \
+--num_train_epochs 100 \
+--logging_dir "/hy-tmp/Projects-AI6132-GENERATIVE-AI-FOR-VISUAL-SYNTHESIS/logs"
